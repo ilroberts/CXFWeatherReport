aff46bf3e56cda26a1485b6454cd9d8e920b375d
illroberts <iwanr@kainos.com>
2017-07-14 11:59:26 +0100

    data being retrieved successfully

--- src/main/java/com/ilroberts/Main.java
+++ src/main/java/com/ilroberts/Main.java
@@ -1,9 +1,17 @@
 package com.ilroberts;
 
 
+import net.webservicex.GlobalWeather;
+import net.webservicex.GlobalWeatherSoap;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        GlobalWeather weather = new GlobalWeather();
+        GlobalWeatherSoap soapReqest = weather.getGlobalWeatherSoap();
+
+        System.out.println(soapReqest.getCitiesByCountry("United Kingdom"));
     }
 }
