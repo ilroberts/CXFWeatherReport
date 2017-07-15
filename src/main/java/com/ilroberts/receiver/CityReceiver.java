bd6e61633fee47206f84e1cfceeb628defc2d402
illroberts <iwanr@kainos.com>
2017-07-15 16:58:31 +0100

    fixed the optional filtering

--- src/main/java/com/ilroberts/receiver/CityReceiver.java
+++ src/main/java/com/ilroberts/receiver/CityReceiver.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
 import com.ilroberts.dto.NewDataSetDTO;
 import com.ilroberts.dto.TableDTO;
+import com.ilroberts.mapper.TableToCityMapper;
 import com.ilroberts.model.City;
 import net.webservicex.GlobalWeather;
 import net.webservicex.GlobalWeatherSoap;
@@ -12,10 +13,9 @@ import net.webservicex.GlobalWeatherSoap;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static com.ilroberts.mapper.TableToCityMapper.mapTableToCity;
 
 public final class CityReceiver implements Receiver<List<City>> {
 
@@ -35,7 +35,11 @@ public final class CityReceiver implements Receiver<List<City>> {
         try {
             NewDataSetDTO dataset = mapper.readValue(cityList, NewDataSetDTO.class);
             Stream<TableDTO> tables = dataset.getTable().stream();
-            return tables.map(t -> mapTableToCity(t).get()).collect(Collectors.toList());
+            return tables
+                    .map(TableToCityMapper::mapTableToCity)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .collect(Collectors.toList());
 
         } catch (IOException e) {
             e.printStackTrace();
